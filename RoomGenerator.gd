501e6189b14c8ddb32f29ce0e84da600995c5e86
Brian-Earl <43723279+Brian-Earl@users.noreply.github.com>
2019-12-12 14:11:29 -0500

    Added Grammers for desk and rugs

--- RoomGenerator.gd
+++ RoomGenerator.gd
@@ -107,16 +107,20 @@ func editDict(var dictionary, var name = null, var letter = null, var keyword = 
 func createDesc(var dictionary):
 	# A simple grammar - or read from a JSON file
 	var grammarTest = Dictionary()
-	grammarTest["bedsentence"] 	= ["The bed has #colour# sheets with a #pillowFeel# pillow."]
+	grammarTest["bedsentence"] = ["The bed has #colour# sheets with a #pillowFeel# pillow."]
+	grammarTest["rugsentence"]= ["A #colour# #rugtype# rug."]
+	grammarTest["desksentence"]= ["A #colour# #woodtype# desk."]
 	grammarTest["pillowFeel"] 		= ["soft", "hard", "flat", "lumpy", "cool", "warm"]
 	grammarTest["colour"]		= ["#tone# #baseColour#"]
 	grammarTest["tone"] 		= ["dark", "light", "pale"]
 	grammarTest["baseColour"] 	= ["red", "green", "blue", "yellow"]
+	grammarTest["rugtype"] = ["shaggy", "wool", "silk", "Persian"]
+	grammarTest["woodtype"] = ["maple","oak","walnut","birch", "mahogany"]
 	var grammar = Tracery.Grammar.new(grammarTest)
 	
 	grammar.addModifiers(Tracery.UniversalModifiers.getModifiers())
 	
-	var desc = grammar.flatten("#bedsentence#");
+	var desc = grammar.flatten("#"+dictionary.name+"sentence#");
 	return desc;
 
 
