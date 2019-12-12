6ac2d463277d4f8e51bf3be22f0beec7b598a966
Brian-Earl <43723279+Brian-Earl@users.noreply.github.com>
2019-12-11 23:51:29 -0500

    Added Main Menu

---
+++ MainMenu.gd
@@ -0,0 +1,15 @@
+extends Node
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func quit():
+	get_tree().quit()
+
+func start():
+	get_tree().change_scene("res://RoomGeneration.tscn")
